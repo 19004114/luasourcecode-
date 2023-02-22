024d52508920ac3789150c1caedc47f1695d5830
minh <minhu170101@gmail.com>
2023-02-22 16:49:22 +0700

    Code Lua 4

--- Bai1.lua
+++ Bai1.lua
@@ -1,45 +1,59 @@
-local tbl = {}
-
-local file = io.open("input.txt","r")
-for line in file:lines() do
-    table.insert(tbl,line)
-end
-
-for i = 1, #tbl do
-    print(tbl[i])
-end
-
-local min = tbl[1]
-local max = tbl[1]
-local t = 0
-local tbc = 0
-
-for i = 1, #tbl do
-    if tonumber(min) > tonumber(tbl[i]) then
-        min = tbl[i]
+function Main()
+    function getLines(file)
+        local check = true
+        local lines = 0
+        while check == true do
+            if read(file,"*l") ~= nil then
+                lines = lines + 1
+            else
+                break
+            end
+        end
+        return lines
     end
-    if tonumber(max) < tonumber(tbl[i]) then
-        max = tbl[i]
+    
+    local tbl = {}
+    
+    local fileO = openfile("input.txt","r")
+    local file = openfile("input.txt","r")
+    local lines = getLines(fileO)
+    
+    for i = 1 , lines do
+        tinsert(tbl,read(file,"*l"))
     end
-    t = t + tbl[i]
-end
-
-tbc = t/#tbl
-
-print("Phan tu nho nhat: " .. min)
-print("Phan tu lon nhat: " .. max)
-print("Trung binh cong cac phan tu: " .. tbc)
-
-io.write("Cac phan tu nho hon trung binh cong: ")
-for i = 1, #tbl do
-    if tonumber(tbl[i]) < tbc then
-        io.write(tbl[i] .. "\t")
+    
+    local min = tbl[1]
+    local max = tbl[1]
+    local t = 0
+    local tbc = 0
+    
+    for i = 1, getn(tbl) do
+        if tonumber(min) > tonumber(tbl[i]) then
+            min = tbl[i]
+        end
+        if tonumber(max) < tonumber(tbl[i]) then
+            max = tbl[i]
+        end
+        t = t + tbl[i]
+    end
+    tbc = t/getn(tbl)
+    
+    print("Phan tu nho nhat: " .. min)
+    print("Phan tu lon nhat: " .. max)
+    print("Trung binh cong cac phan tu: " .. tbc)
+    
+    write("Cac phan tu nho hon trung binh cong: ")
+    for i = 1, getn(tbl) do
+        if tonumber(tbl[i]) < tbc then
+            write(tbl[i] .. "\t")
+        end
+    end
+    
+    write("\nCac phan tu lon hon trung binh cong: ")
+    for i = 1, getn(tbl) do
+        if tonumber(tbl[i]) > tbc then
+            write(tbl[i] .. "\t")
+        end
     end
 end
-
-io.write("\nCac phan tu lon hon trung binh cong: ")
-for i = 1, #tbl do
-    if tonumber(tbl[i]) > tbc then
-        io.write(tbl[i] .. "\t")
-    end
-end
+Main()
--- Bai2.lua
+++ Bai2.lua
@@ -1,95 +1,104 @@
-function file_exists(file)
-    local f = io.open(file, "rb")
-    if f then 
-        f:close() 
+function Main()
+    function getLines(file)
+        local check = true
+        local lines = 0
+        while check == true do
+            if read(file,"*l") ~= nil then
+                lines = lines + 1
+            else
+                break
+            end
+        end
+        return lines
     end
-    return f ~= nil
-end
-function lines_from(file)
-    if not file_exists(file) then return {} end
-    local lines = {}
-    for line in io.lines(file) do 
-      lines[#lines + 1] = line
+    function find_name(name)
+        local t = ""
+        local tbl = {}
+        gsub(name, "(%w+)", function (w) tinsert(%tbl, w) end)
+        t = tbl[getn(tbl)]
+        return strlower(t)
     end
-    return lines
-end
-function find_name(name)
-    local t = ""
-    local tbl = {}
-    for i in string.gmatch(name, "%a+") do
-        table.insert(tbl,i)
+    
+    local tbd = {}
+    
+    local fileO = openfile("input2.txt","r")
+    local file = openfile("input2.txt","r")
+    local file2O = openfile("input3.txt","r")
+    local file2 = openfile("input3.txt","r")
+    
+    local lines = getLines(fileO)
+    local lines2 = getLines(file2O)
+    
+    for i = 1 , lines do
+        tinsert(tbd,read(file,"*l"))
     end
-    for i = #tbl, #tbl do
-        t = tbl[i]
+    
+    local dshs = {}
+    for i = 1, getn(tbd),2 do
+        tinsert(dshs, {diem = tbd[i], name = tbd[i+1]})
     end
-    return string.lower(t)
-end
-
-
-local file = 'input2.txt'
-local lines = lines_from(file)
-local dshs = {}
-
-for i = 1, #lines,2 do
-    table.insert(dshs, {diem = lines[i], name = lines[i+1]} )
-end
-
-print("Danh sach hoc sinh: ")
-for i = 1, #dshs do
-    print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
-end
-
-local tt = ""
-local td = 0
-for i = 1, #dshs do
-    if i ~= #dshs then
-        for j = i + 1, #dshs do
-            if find_name(dshs[i].name) > find_name(dshs[j].name) then
-                tt = dshs[i].name
-                dshs[i].name = dshs[j].name
-                dshs[j].name = tt
-                
-                td = dshs[i].diem
-                dshs[i].diem = dshs[j].diem
-                dshs[j].diem = td
+    
+    print("Danh sach hoc sinh: ")
+    for i = 1, getn(dshs) do
+        print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
+    end
+    
+    local tt = ""
+    local td = 0
+    for i = 1, getn(dshs) do
+        if i ~= getn(dshs) then
+            for j = i + 1, getn(dshs) do
+                if find_name(dshs[i].name) > find_name(dshs[j].name) then
+                    tt = dshs[i].name
+                    dshs[i].name = dshs[j].name
+                    dshs[j].name = tt
+                    
+                    td = dshs[i].diem
+                    dshs[i].diem = dshs[j].diem
+                    dshs[j].diem = td
+                end
             end
         end
     end
-end
-
-print("Danh sach hoc sinh sau khi sap xep: ")
-for i = 1, #dshs do
-    print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
-end
-
-local file2 = 'input3.txt'
-local lines2 = lines_from(file2)
-for i = 1, #lines2,2 do
-    table.insert(dshs, {diem = lines2[i], name = lines2[i+1]} )
-end
-
-print("Danh sach hoc sinh sau khi them: ")
-for i = 1, #dshs do
-    print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
-end
-
-for i = 1, #dshs do
-    if i ~= #dshs then
-        for j = i + 1, #dshs do
-            if find_name(dshs[i].name) > find_name(dshs[j].name) then
-                tt = dshs[i].name
-                dshs[i].name = dshs[j].name
-                dshs[j].name = tt
-                
-                td = dshs[i].diem
-                dshs[i].diem = dshs[j].diem
-                dshs[j].diem = td
+    
+    print("Danh sach hoc sinh sau khi sap xep: ")
+    for i = 1, getn(dshs) do
+        print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
+    end
+    
+    local tbNew = {}
+    for i = 1 , lines2 do
+        tinsert(tbNew,read(file2,"*l"))
+    end
+    
+    for i = 1, getn(tbNew),2 do
+        tinsert(dshs, {diem = tbNew[i], name = tbNew[i+1]})
+    end
+    
+    print("Danh sach hoc sinh sau khi them: ")
+    for i = 1, getn(dshs) do
+        print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
+    end
+    
+    for i = 1, getn(dshs) do
+        if i ~= getn(dshs) then
+            for j = i + 1, getn(dshs) do
+                if find_name(dshs[i].name) > find_name(dshs[j].name) then
+                    tt = dshs[i].name
+                    dshs[i].name = dshs[j].name
+                    dshs[j].name = tt
+                    
+                    td = dshs[i].diem
+                    dshs[i].diem = dshs[j].diem
+                    dshs[j].diem = td
+                end
             end
         end
     end
+    
+    print("Danh sach hoc sinh moi sau khi sap xep: ")
+    for i = 1, getn(dshs) do
+        print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
+    end
 end
-
-print("Danh sach hoc sinh moi sau khi sap xep: ")
-for i = 1, #dshs do
-    print("Ho ten: " .. dshs[i].name .. "\tDiem: " .. dshs[i].diem)
-end
+Main()
